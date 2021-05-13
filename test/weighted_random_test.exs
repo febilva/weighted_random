51bea3279b4d870a4668f464c037b766119c1b67
Febil VA <febil.tvr@gmail.com>
2021-05-13 10:04:28 +0530

    fix test

--- test/weighted_random_test.exs
+++ test/weighted_random_test.exs
@@ -8,11 +8,11 @@ defmodule WeightedRandomTest do
 
   test "return the random one" do
     items = [apples: 10, oranges: 30, bananas: 60]
-    random_list = 1..10_000_00 |> Enum.map(fn x -> WeightedRandom.weight_based_choice(items) end) |> IO.inspect()
+    random_list = 1..10_000_00 |> Enum.map(fn x -> WeightedRandom.weight_based_choice(items) end)
     percentage_banana = random_list |> Enum.count(fn x -> x == :bananas end)
 
 
-    assert percentage_banana/10_000_00 == 0.6
+    assert percentage_banana/10_000_00 |> Float.round(1) == 0.6
     # assert WeightedRandom.weight_based_choice(items) == %{}
   end
 end
